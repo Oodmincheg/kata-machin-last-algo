ae4f2b20cfcef8122c187270331c1cefdbb30f15
Vorobiov Vladyslav <vorobyov.vladislav@gmail.com>
2023-11-04 21:27:36 +0200

    binary search done. video watched.

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+   let low = 0;
+   let high = haystack.length
+    while(low < high) {
+       const m = Math.floor(low + (high - low) / 2)
+        if(haystack[m] === needle) {
+        return true
+    }
+        if(haystack[m] > needle) {
+            high = m
+        } else {
+            low = m + 1
+        }
 
+
+    }
+    return false
 }
+
