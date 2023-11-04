19a67e628087e817f7b51ba0d4219f8c42d91ef4
Vorobiov Vladyslav <vorobyov.vladislav@gmail.com>
2023-11-04 22:37:54 +0200

    two balls problem done.

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,16 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let ballsAmounts = 2;
+    const step = Math.sqrt(breaks.length)
+    let prev = 0
+    let next = prev + Math.floor(step)
 
+    while(next < breaks.length) {
+        if(breaks[next]) {
+            for(let i = prev; i < next; i++) {
+                if(breaks[i]) return i
+            }
+        }
+        [prev, next] = [next, next + step]
+    }
+    return -1
 }
