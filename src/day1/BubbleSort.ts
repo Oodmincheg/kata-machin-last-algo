9dc50af882518e146072a63ecb7da420e0603e4d
Vorobiov Vladyslav <vorobyov.vladislav@gmail.com>
2023-11-04 22:38:05 +0200

    bubble sort done.

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,16 @@
 export default function bubble_sort(arr: number[]): void {
-
+    let switches = true;
+    let end = arr.length
+    while(switches) {
+        let i = 0;
+        switches = false;
+        while(i < end - 1) {
+            if(arr[i] > arr[i+1]){
+                [arr[i], arr[i+1]] = [arr[i+1], arr[i]]
+                switches = true
+            }
+            i++
+        }
+        end--
+    }
 }
