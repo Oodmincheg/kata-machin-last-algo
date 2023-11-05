1a1f26e6fa5c08946c31bfb835757cb0d40eeff1
Vorobiov Vladyslav <vorobyov.vladislav@gmail.com>
2023-11-05 15:20:20 +0200

    finish queue. watch video two times. foret clean tail if queue is empty.

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,42 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>
+}
 export default class Queue<T> {
     public length: number;
+    private head: Node<T> | undefined;
+    private tail: Node<T> | undefined;
 
-    
 
     constructor() {
+        this.length = 0
     }
 
     enqueue(item: T): void {
-
+        console.log('was enqueue ')
+        const node = {value: item}
+        this.length++
+        if(!this.tail) {
+            this.tail = this.head = node
+            return
+        }
+        this.tail.next = node
+        this.tail = node
 }
     deque(): T | undefined {
-
+        if(!this.head) {
+            console.log('empty head. length is ', this.length)
+            return
+        }
+        this.length--
+        if(this.length === 0) {
+            this.tail = undefined
+        }
+        const result = this.head.value
+        this.head = this.head.next
+        return result
 }
     peek(): T | undefined {
-
+        return this.head?.value
 }
 }
