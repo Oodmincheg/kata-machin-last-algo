1de5029094de3494f562c9bd2a64db87cbc6e90e
Vorobiov Vladyslav <vorobyov.vladislav@gmail.com>
2023-11-05 15:22:06 +0200

    finish stack. without watching any video.

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,37 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>
+}
 export default class Stack<T> {
     public length: number;
+    private tail: Node<T> | undefined
 
-    
 
     constructor() {
+        this.length = 0
     }
 
     push(item: T): void {
+        const node = {value: item}
+        this.length++
+        if(!this.tail) {
+            this.tail = node
+            return
+        }
 
+        const newNext = this.tail
+        this.tail = {...node, next: newNext}
 }
     pop(): T | undefined {
-
+        if(!this.tail) {
+            return
+        }
+        this.length--
+        const result = this.tail.value
+        this.tail = this.tail.next
+        return result
 }
     peek(): T | undefined {
-
+        return this.tail?.value
 }
 }
